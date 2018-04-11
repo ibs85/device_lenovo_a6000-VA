381b97ee5f7329d86600742983eb4fd5f38cf962
ibs85 <35095745+ibs85@users.noreply.github.com>
2018-04-11 10:15:28 +0530

    Update and rename screwd_a6000.mk to nitrogen_a6000.mk

--- nitrogen_a6000.mk
+++ nitrogen_a6000.mk
@@ -15,8 +15,8 @@
 
 $(call inherit-product, device/lenovo/a6000/full_a6000.mk)
 
-# Include Screwd common configuration
-include vendor/screwd/main.mk
+# Inherit some common Nitrogen stuff.
+$(call inherit-product, vendor/nitrogen/products/common.mk)
 
 # Must define platform variant before including any common things
 TARGET_BOARD_PLATFORM_VARIANT := msm8916
@@ -24,7 +24,7 @@ TARGET_BOARD_PLATFORM_VARIANT := msm8916
 # Assert
 TARGET_OTA_ASSERT_DEVICE := Kraft-T,a6000,K30-T,A6000,Kraft-W,Kraft-C,k30t,msm8916,Kraft-A6000,wt86518
 
-PRODUCT_NAME := screwd_a6000
+PRODUCT_NAME := nitrogen_a6000
 PRODUCT_DEVICE := a6000
 PRODUCT_BRAND := Lenovo
 PRODUCT_MODEL := a6000
@@ -37,3 +37,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="Kraft-A6000-user 5.0.2 LRX22G Kraft-A6000_S061_160727 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-lenovo
+
+# Boot animation
+TARGET_SCREEN_WIDTH := 720
+TARGET_SCREEN_HEIGHT := 1280
